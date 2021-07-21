2db0dfb8765b3f09c7fdd190c607ba13b9f7dfc3
vikash saini <vkrsaini678@gmail.com>
2021-07-22 00:15:10 +0530

    fixed window resize position bug

--- src/Popover.tsx
+++ src/Popover.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
+
 import React, {
   useRef,
   useLayoutEffect,
@@ -189,7 +190,7 @@ export const Popover = forwardRef<HTMLElement, PopoverProps>(
     );
 
     const handleWindowResize = useCallback(() => {
-      window.requestAnimationFrame(positionPopover);
+      window.requestAnimationFrame(() => positionPopover);
     }, [positionPopover]);
 
     useEffect(() => {
